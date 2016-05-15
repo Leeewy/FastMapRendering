924f7b4ac5e75a3604b4542cb1450ce526319ed2
Lewy <leeewy@gmail.com>
2016-05-15 11:22:51 +0200

    Move add markers (10 000) to async task

--- app/src/main/java/com/lewy/googlemapsutil/MainActivity.java
+++ app/src/main/java/com/lewy/googlemapsutil/MainActivity.java
@@ -1,11 +1,11 @@
 package com.lewy.googlemapsutil;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,15 +15,15 @@ import com.google.android.gms.maps.model.LatLng;
 import com.lewy.googlemapsutil.managers.FastClusterManager;
 import com.lewy.googlemapsutil.managers.MyDefaultClusterRenderer;
 
-import org.json.JSONException;
-
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private FastClusterManager<MapItem> mFastClusterManager;
 
     private GoogleMap mMap;
 
-    LatLng latLng = new LatLng(54.32, 18.64);
+    private LatLng latLng = new LatLng(54.32, 18.64); // GDAŃSK
+
+    private boolean mManyMarkers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +46,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mFastClusterManager = new FastClusterManager<>(this, mMap, true);
 
         mMap.setOnCameraChangeListener(mFastClusterManager);
-        try {
-            readItems();
-        } catch (JSONException e) {
-            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
-        }
+
+        new CreateMarkers().execute();
 
         setSwitchAction();
     }
@@ -59,22 +56,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Switch switchView = (Switch) findViewById(R.id.faster_cluster_switch);
         switchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    mFastClusterManager.setRenderer(new MyDefaultClusterRenderer(MainActivity.this, mMap, mFastClusterManager, true));
-                } else {
-                    mFastClusterManager.setRenderer(new MyDefaultClusterRenderer(MainActivity.this, mMap, mFastClusterManager, false));
-                }
+                mManyMarkers = isChecked;
+                new CreateMarkers().execute();
             }
         });
     }
 
-    private void readItems() throws JSONException {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                LatLng newLatLng = new LatLng(latLng.latitude + ((double) i) / 1000, latLng.longitude + ((double) j) / 1000);
-                MapItem mapItem = new MapItem(newLatLng.latitude, newLatLng.longitude);
-                mFastClusterManager.addItem(mapItem);
+    class CreateMarkers extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            mFastClusterManager.clearItems();
+            for (int i = 0; i < 100; i++) {
+                for (int j = 0; j < 100; j++) {
+                    LatLng newLatLng = new LatLng(latLng.latitude + ((double) i) / 1000, latLng.longitude + ((double) j) / 1000);
+                    MapItem mapItem = new MapItem(newLatLng.latitude, newLatLng.longitude);
+                    mFastClusterManager.addItem(mapItem);
+                }
             }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mFastClusterManager.clearItems();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mFastClusterManager.setRenderer(new MyDefaultClusterRenderer(MainActivity.this, mMap, mFastClusterManager, mManyMarkers));
         }
     }
 }
